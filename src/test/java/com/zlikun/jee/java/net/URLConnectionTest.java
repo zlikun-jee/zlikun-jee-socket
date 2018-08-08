9d7c18a1e3891eb36e4d85616fc6ce48443603c0
zlikun <zlikun-dev@hotmail.com>
2018-08-08 20:41:18 +0800

    测试URLConnection语法（简略）

--- src/test/java/com/zlikun/jee/java/net/URLConnectionTest.java
+++ src/test/java/com/zlikun/jee/java/net/URLConnectionTest.java
@@ -1,8 +1,31 @@
 package com.zlikun.jee.java.net;
 
+import org.junit.Test;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URI;
+import java.net.URLConnection;
+
 /**
  * @author zlikun <zlikun-dev@hotmail.com>
  * @date 2018/8/8 20:12
  */
 public class URLConnectionTest {
+
+    @Test
+    public void test() throws IOException {
+
+        URLConnection connection = URI.create("https://zlikun.com").toURL().openConnection();
+
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                System.out.println(line);
+            }
+        }
+
+    }
+
 }
